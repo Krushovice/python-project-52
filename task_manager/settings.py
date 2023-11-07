f5ab51789ce875dc58d3f97c9179cb140469f9c6
Krushovice <Kickstar69@yandex.ru>
2023-11-07 10:20:41 +0000

    Удалено лишнее из settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.getenv('RENDER')
+DEBUG_ENV = os.getenv('DEBUG')
+DEBUG = DEBUG_ENV if DEBUG_ENV else False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'webserver']
 
@@ -126,16 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Following settings only make sense on production and may break development environments.
-if not DEBUG:
-    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-    # Turn on WhiteNoise storage backend that takes care of compressing static files
-    # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
