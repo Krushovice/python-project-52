f10410fc9acfede1d7521106e72543a5ecc98231
Krushovice <Kickstar69@yandex.ru>
2023-11-07 07:53:37 +0000

    Создана главная страница в тестовом приложении

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'webserver']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'task_manager',
+    'task_manager.tasks',
 ]
 
 MIDDLEWARE = [
