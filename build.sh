384d2d0486b50050393906fb8604c4e8b31a6148
Krushovice <Kickstar69@yandex.ru>
2023-11-07 10:01:45 +0000

    fix poetry

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 python3 -m pip install --upgrade pip
+python3 -m install poetry==1.7.0
+rm poetry.lock
+poetry lock
 poetry install
 
 poetry run python manage.py collectstatic --no-input
