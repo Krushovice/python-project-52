267b01c385dd6a949f6c474a40941770093574af
Krushovice <Kickstar69@yandex.ru>
2023-11-26 01:53:12 +0000

    Исправлена админ панель

--- task_manager/users/admin.py
+++ task_manager/users/admin.py
@@ -1,15 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-
-from .forms import CustomUserCreationForm, CustomUserUpdateForm
 from .models import CustomUser
 
 
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserUpdateForm
-    model = CustomUser
-    list_display = ['first_name', 'last_name', 'password',]
-
-
-admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(CustomUser)
