f3523c95fd6802bf736fa7d9d869622b727b068d
Krushovice <Kickstar69@yandex.ru>
2023-11-27 07:48:08 +0000

    Начал писать тесты для приложения users

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import CustomUser as User
+
 
 # Create your tests here.
+class TestUser(TestCase, Client):
+    def __init__(self, *args, **kwargs):
+        self.client = Client()
+
+    def setUp(self):
+        User.objects.create(first_name='Frank', last_name='Abignel', username='Aviator', password=123456)
+
+    def test_login(self):
+        pass
