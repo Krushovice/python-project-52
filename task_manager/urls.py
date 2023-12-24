5856a66cedde4d4f01664dddb95ebd4f60a88da8
Krushovice <Kickstar69@yandex.ru>
2023-12-24 05:49:39 +0000

    Добавлен текст успешного выхода из системы

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,17 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth.views import LogoutView
-from django.urls import path, include, reverse_lazy
+from django.urls import path, include
 from task_manager import views
 
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('admin/', admin.site.urls),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(next_page=reverse_lazy('index')),
-         name='logout'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
     path('users/', include('task_manager.user.urls')),
     path('statuses/', include('task_manager.status.urls')),
     path('labels/', include('task_manager.label.urls')),
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import View
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.utils.translation import gettext_lazy as _
+from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 
 
@@ -11,7 +12,7 @@ class IndexView(View):
         return render(request, 'index.html')
 
 
-class LoginView(SuccessMessageMixin, LoginView):
+class CustomLoginView(SuccessMessageMixin, LoginView):
     template_name = 'users/login.html'
     next_page = reverse_lazy('index')
     success_message = _("You are logged in")
@@ -25,3 +26,13 @@ class LoginView(SuccessMessageMixin, LoginView):
             self.initial = {'username': autofill_username}
 
         return super().get(request, *args, **kwargs)
+
+
+class CustomLogoutView(SuccessMessageMixin, LogoutView):
+    next_page = reverse_lazy('index')
+
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        msg_text = _("You are logged out")
+        messages.info(self.request, msg_text)
+        return response
