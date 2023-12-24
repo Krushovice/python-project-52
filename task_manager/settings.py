e22bd7a0b83bf9d2b691e68da1155f24d29da592
Krushovice <Kickstar69@yandex.ru>
2023-12-24 05:23:32 +0000

    fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -146,11 +146,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 LANGUAGES = [
     ('en-us', 'English'),
-    ('ru-ru', 'Russian'),
+    ('ru-RU', 'Russian'),
 ]
 
 LOCALE_PATHS = [
